c9ac8bb8a6fc8e0c88d196f567183d897e64b108
Anthony <haowanxing@users.noreply.github.com>
2026-04-28 12:26:50 +0800

    fix: install only .ko files to avoid directory install error

--- general/package/sigmastar-osdrv-sensors/sigmastar-osdrv-sensors.mk
+++ general/package/sigmastar-osdrv-sensors/sigmastar-osdrv-sensors.mk
@@ -11,7 +11,7 @@ SIGMASTAR_OSDRV_SENSORS_DEPENDENCIES = linux
 
 define SIGMASTAR_OSDRV_SENSORS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/sigmastar
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/sigmastar $(@D)/sigmastar/$(OPENIPC_SOC_FAMILY)/*
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/sigmastar $(@D)/sigmastar/$(OPENIPC_SOC_FAMILY)/*.ko
 endef
 
 $(eval $(generic-package))
